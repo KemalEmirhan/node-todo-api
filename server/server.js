94e640e1c13d12647b595c90aa29246a675eb449
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2018-05-02 17:36:16 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -28,3 +28,4 @@ app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,58 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it('Should create a new todo', (done) => {
+    let text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((response) => {
+        expect(response.body.text).toBe(text);
+      })
+      .end((error, response) => {
+        if(error) {
+          return done(error);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((error) => {
+          done(error);
+        });
+      });
+  });
+
+  it('Should not create todo with invalid body data', (done) => {
+    
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((error, response) => {
+        if (error) {
+          return done(error);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((error) => {
+          done(error);
+        });
+      });
+  });
+
+  
+});
