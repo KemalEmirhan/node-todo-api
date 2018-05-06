1921c796ebce6f4a41242150c8d5a3724c58e7c6
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2018-05-06 21:42:28 +0300

    Modified queries playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,35 +1,49 @@
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');  
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-let id = '5aeda0e3400a100d8f75baae';
+// let id = '5aeda0e3400a100d8f75baae';
 
-Todo.find({
-  _id: id
-}).then((todos) => {
-  if (!todos) {
-    return console.log('Id not found.');
-  } else {
-    console.log('Todos', todos);
-  }
-});
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
 
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  if (!todo) {
-    console.log('Id not found.');
-  } else {
-    console.log('Todo', todo);
-  }
-});
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   if (!todos) {
+//     return console.log('Id not found.');
+//   } else {
+//     console.log('Todos', todos);
+//   }
+// });
 
-Todo.findById(id)
-  .then((todo) => {
-    if (!todo) {
-      return console.log('Id not found.');
-    } else {
-      console.log('Todo', todo);
-    }
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   if (!todo) {
+//     console.log('Id not found.');
+//   } else {
+//     console.log('Todo', todo);
+//   }
+// });
+
+// Todo.findById(id)
+//   .then((todo) => {
+//     if (!todo) {
+//       return console.log('Id not found.');
+//     } else {
+//       console.log('Todo', todo);
+//     }
+//   });
+const id = '5ae9c1b819941e065a079792';
+
+if (ObjectID.isValid(id)) {
+  User.findById(id).then((user) => {
+    console.log(JSON.stringify(user, undefined, 2));
   });
-
-
+} else {
+  console.log('ID is not valid!');
+};
