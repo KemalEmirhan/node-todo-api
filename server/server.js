b57508d85133d0ea450b4f398ca2bcb8f67ef705
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2018-05-06 22:01:13 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Importing Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 // Importing Local Files
 const {mongoose} = require('./db/mongoose');
@@ -30,6 +31,27 @@ app.get('/todos', (request, response) => {
   }, (error) => {
     response.status(400).send(error);
   });
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (request, response) => {
+  let id = request.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  } else {
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        response.status(404).send();
+      } 
+      response.send({todo});
+      
+    }).catch((error) => {
+      console.log(error);
+      response.status(400).send();
+    });
+  }
+
 });
 
 app.listen(3000, () => {
