33b0d7b6cceed1ddfcaa4c5bcd5fbab1f3731743
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2018-05-16 14:33:01 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,17 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+
 UserSchema.statics.findByToken = function (token) {
   let User = this;
   let decoded;
--- server/server.js
+++ server/server.js
@@ -133,6 +133,15 @@ app.post('/users/login', (request, response) => {
   });
 });
 
+app.delete('/users/me/token', authenticate, (request, response) => {
+  request.user.removeToken(request.token).then(() => {
+    response.status(200).send();
+  }, () => {
+    response.status(400).send();
+  })
+});
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
