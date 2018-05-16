35bdd642b0cd9e032f8831d91a2debcc9142f015
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2018-05-16 11:18:01 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,28 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (error, response) => {
+        if (response) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
 UserSchema.pre('save', function(next) {
   let user = this;
 
--- server/server.js
+++ server/server.js
@@ -121,6 +121,18 @@ app.get('/users/me', authenticate, (request, response) => {
   response.send(request.user);
 });
 
+app.post('/users/login', (request, response) => {
+  var body = _.pick(request.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      response.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    response.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
