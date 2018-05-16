cd86b6fc9d0b26aa69491250f77c54e19d123cb3
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2018-05-16 14:23:11 +0300

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -229,6 +229,8 @@ describe('POST /users', () => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
+        }).catch((error) => {
+          done(error);
         });
       });
   });
@@ -256,3 +258,58 @@ describe('POST /users', () => {
   });
 });
 
+
+describe('POST /users/login', () => {
+  it('Should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((response) => {
+        expect(response.headers['x-auth']).toBeTruthy();
+      })
+      .end((error, response) => {
+        if (error) {
+          return done(error);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toMatchObject({
+            access: 'auth',
+            token: response.headers['x-auth']
+          });
+          done();
+        }).catch((error) => {
+          done(error);
+        });
+      });
+  });
+
+  it('Should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: '123'
+      })
+      .expect(400)
+      .expect((response) => {
+        expect(response.headers['x-auth']).toBeFalsy();
+      })
+      .end((error, response) => {
+        if (error) {
+          return done(error);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((error) => {
+          done(error);
+        });
+      });
+  });
+});
